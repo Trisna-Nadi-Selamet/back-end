e92132ed13e906b38f46295bdeec8781442aa57f
Dies Nadi <nadi.selamet@gmail.com>
2021-08-03 15:04:49 +0700

    learning Golang

--- golang-dasar/array.go
+++ golang-dasar/array.go
@@ -1,12 +1,11 @@
 package main
 
-import "fmt"
-
-func main(){
+/*func main(){
     var buah=[4]string{"Apel","Jeruk","Nangka","Pisang"}
 	//untuk merubah value pada array di golang hanya menyebutkan nilai index saja
 	buah[1]="Nanas"
-	
+
 	fmt.Println("Jumlah elment :",len(buah))
 	fmt.Println("Isi elment :",buah)
 }
+*/
---
+++ golang-dasar/slice.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+//perbedaan slice dan array adalah pada saat deklarasi di awal, array sudah ditentukan sedangkan slice tidak ditentukan
+func main(){
+    var buah = []string{"apel","pisang","nangka","jambu"}
+
+	//penambahan value slice menggunakan append
+	buah = append(buah,"nanas")
+    
+	fmt.Println(buah)
+	fmt.Println("jumlah elment : ",len(buah))
+
+}
