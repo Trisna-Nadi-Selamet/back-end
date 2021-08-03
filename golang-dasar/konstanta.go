2193ee86a379892b79421d1024e3897a9ef8287f
Dies Nadi <nadi.selamet@gmail.com>
2021-08-03 10:45:38 +0700

    learn Golang

---
+++ golang-dasar/konstanta.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+//mengenal konstanta atau tetapan adalah suatu nilai tetap
+func main(){
+    const x = 10
+
+	fmt.Printf("%d",x)
+
+}
--- golang-dasar/tipedata.go
+++ golang-dasar/tipedata.go
@@ -1,25 +1,32 @@
 package main
 
-import "fmt"
-
 //tipe data golang positife ,negative , desimal,boolean:
-func main(){
+
+/*func main(){
 	//positif uint8 angka dari 0 - 255
-	var number_positif uint8 = 255
-	//positif uint16 angka dari 0 - 65535  
+	var number_positif1 uint8 = 255
+	//positif uint16 angka dari 0 - 65535
     var number_positif2 uint16 = 65535
-	//positif uint32 angka dari 0 - 4294967293  
+	//positif uint32 angka dari 0 - 4294967293
     var number_positif3 uint32 = 4294967293
-    //positif uint64 angka dari 0 - 18446744073709557675  
+    //positif uint64 angka dari 0 - 18446744073709557675
     var number_positif4 uint64 = 4294967293
 
 	//negative angka -128 - 127
-    var number_negaif int8 =-127 
+    var number_negaif int8 =-127
 
 	//desimal penggunaan menggunakan titik(.)
 	var number_decimal = 2.5
 
-	fmt.Println(number_positif,number_positif2,number_positif3,number_positif4)
-    fmt.Println(number_negaif)
+	//boolean penggunaan menggunakan true or false
+	var apakah_ada bool = true
+
+	fmt.Println(number_positif1)
+    fmt.Println(number_positif2)
+	fmt.Println(number_positif3)
+	fmt.Println(number_positif4)
+	fmt.Println(number_negaif)
 	fmt.Println(number_decimal)
+	fmt.Println(apakah_ada)
 }
+*/
