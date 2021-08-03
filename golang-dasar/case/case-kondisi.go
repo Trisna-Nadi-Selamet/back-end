497c1afc23ad0c49f275782b63c444f44c90b2c6
Dies Nadi <nadi.selamet@gmail.com>
2021-08-03 13:16:52 +0700

    go

---
+++ golang-dasar/case/case-kondisi.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	var angka = 4
+	if angka % 2 == 0   {
+		fmt.Printf("bilangan genap %d" , angka)
+	}else{
+		fmt.Printf("bilangan ganjil %d" , angka)
+	}
+	
+}
