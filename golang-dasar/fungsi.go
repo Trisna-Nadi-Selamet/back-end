4e2703e4524f8a031dd1eafafea90631fdf32235
Dies Nadi <nadi.selamet@gmail.com>
2021-08-04 20:42:44 +0700

    learning Golang

--- golang-dasar/fungsi.go
+++ golang-dasar/fungsi.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func main(){
+
+    var num1,num2 = bilangan_hasil(20,5)
 	fmt.Println(tampilkan_pesan())
 	fmt.Println(tampilkan_angka())
 	fmt.Println(tampilkan_hasil(10,5))
+	fmt.Println(num1)
+	fmt.Println(num2)
 }
 func tampilkan_pesan()string{ //menggunakan tipe variabel
 	return "Berhasil diterima"  // menampilkan dengan cara mengembalikan nilai
@@ -22,4 +26,11 @@ func tampilkan_hasil(x int,y int)int{
 	var z = x + y
 	return z
 
+}
+
+//menampilkan pengembalian multi fungsi pada parameter pada golang
+func bilangan_hasil(a int, b int)(int,int){ //syarat penulisan untuk pengembalian multi fungsi
+	var c = a + b;
+	var d = a * b;
+	return c,d
 }
